7cc103deae5b6fde2314036111eda6c9cb564590
D Tucker <danielle@tuckerlabs.com>
2013-08-23 23:47:14 -0700

    Change date display styling

--- config/initializers/rails_defaults.rb
+++ config/initializers/rails_defaults.rb
@@ -1,4 +1,4 @@
 #Be sure to restart the rails server after modifying this file
 
-Date::DATE_FORMATS[:default] = '%m-%d-%Y'
-Time::DATE_FORMATS[:default] = '%m-%d-%Y %I:%M %p'
+Date::DATE_FORMATS[:default] = '%m/%d/%Y'
+Time::DATE_FORMATS[:default] = '%m/%d/%Y %I:%M %p'
